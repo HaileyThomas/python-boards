e880145696947ff84960fdf125006fecd980d0d4
HaileyThomas <haileyraebeauty@gmail.com>
2022-05-09 17:14:26 -0700

    add login and logout features

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from app.routes import home, dashboard
+from app.routes import home, dashboard, api
 from app.db import init_db
 from app.utils import filters
 
@@ -15,6 +15,7 @@ def create_app(text_config=None):
     # register routes
     app.register_blueprint(home)
     app.register_blueprint(dashboard)
+    app.register_blueprint(api)
 
     # add filters
     app.jinja_env.filters['format_date'] = filters.format_date
